5b35d8057278d51cbfc1fd07c276028c9ee044b3
Uvik <uvik.ren@gmail.com>
2015-04-06 10:49:32 +0200

    dupe SMS removal works

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # About this project
-Small C++ project to delete duplicate messages (SMS or MMS) from a SMS Backup&amp;Restore backup.
-Using Lee Thomason's TinyXML2 library for XML parsing.
+Dupe Buster is a small C++ project to delete duplicate messages (SMS or MMS) from a SMS Backup&amp;Restore backup.
+It uses Lee Thomason's TinyXML2 library for XML parsing.
 This project is still a *work in progress* (and incidentally, my first Github project :) )
 
 # License
--- find_dupes.cpp
+++ find_dupes.cpp
@@ -39,7 +39,15 @@ bool equalNumbers( const char* str1, const char* str2 )
     return !strcmp( pos1, pos2 );
 }
 
-bool equalElements( const XMLElement * element1, const XMLElement * element2 )
+bool equalSMSes( const XMLElement * sms1, const XMLElement * sms2 )
+{
+    return equalNumbers( sms1->Attribute("address"), sms2->Attribute("address") )
+        || !strcmp( sms1->Attribute("date"), sms2->Attribute("date") )
+        || !strcmp( sms1->Attribute("type"), sms2->Attribute("type") )
+        || !strcmp( sms1->Attribute("body"), sms2->Attribute("body") );
+}
+
+bool equalMMSes( const XMLElement * mms1, const XMLElement * mms2 )
 {
     return false;
 }
@@ -49,11 +57,27 @@ int main( void )
     XMLDocument doc;
     doc.LoadFile( "small_sms.xml" );
 
-    const XMLNode * curSMS = doc.FirstChildElement( "smses" )->FirstChildElement("sms");
-    cout << curSMS->ToElement()->Attribute("address") << endl;
+    int MAX_COUNT = 8;
+    int i = 0;
 
-    const XMLNode * nextSMS = curSMS->NextSiblingElement("sms");
-    cout << nextSMS->ToElement()->Attribute("address") << endl;
+    XMLNode * smses = doc.FirstChildElement( "smses" );
+    if ( !smses ) return 0;
+
+    XMLNode * curSMS = smses->FirstChildElement( "sms" );
+    do
+    {
+        XMLNode * nextSMS = curSMS->NextSiblingElement("sms");
+        if ( nextSMS != NULL && equalSMSes( curSMS->ToElement(), nextSMS->ToElement() ) )
+            smses->DeleteChild( nextSMS );
+
+        curSMS = curSMS->NextSiblingElement("sms");
+    } while ( i < MAX_COUNT && curSMS != NULL );
+
+
+    //const XMLNode * nextSMS = curSMS->NextSiblingElement("sms");
+    //cout << nextSMS->ToElement()->Attribute("address") << endl;
+
+    doc.SaveFile("new.xml");
 
     return 1;
 }
